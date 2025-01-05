767bc1de8be9a1e62a1b70bd04fb2cd123ccb31f
vinay kumar maurya <113638115+vinne01@users.noreply.github.com>
2025-01-06 02:23:51 +0530

    Update settings.py

--- chaiheadq/settings.py
+++ chaiheadq/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
- STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
